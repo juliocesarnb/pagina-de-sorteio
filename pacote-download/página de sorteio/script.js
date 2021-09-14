c1ea823726ac2cc814562c05f4ba14e847d4312d
Júlio Cesar <jullliocesarbb@hotmail.com>
2021-09-14 13:25:07 -0300

    raffle page

---
+++ pacote-download/página de sorteio/script.js
@@ -0,0 +1,8 @@
+function draw(){
+    let n = document.getElementById("numberdraw").value;
+
+    let draw = Math.round(Math.random() * n );
+
+    document.getElementById("result").innerHTML = draw;
+
+}
